01d8870a6e2d976c8738eb49b922413418df438a
Andrew Allbright <aallbrig@gmail.com>
2021-11-01 23:22:13 -0400

    Spice tile flashes white, screen shakes, and more points are awarded for eating spice

--- unity/dune-sandworm-micro-prototype/Assets/Scripts/Behaviors/Spice.cs
+++ unity/dune-sandworm-micro-prototype/Assets/Scripts/Behaviors/Spice.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Cinemachine;
 using UnityEngine;
 
 namespace Behaviors
@@ -8,14 +10,27 @@ namespace Behaviors
         [SerializeField] private float points = 1f;
         [SerializeField] private float eatDelay = 1f;
         [SerializeField] private ParticleSystem onBeEatenParticles;
+        [SerializeField] private MeshRenderer meshRenderer;
+        [SerializeField] private float flashDelay = 0.16f;
+        [SerializeField] private float flashCount = 2;
+        [SerializeField] private CinemachineImpulseSource impulseSource;
+
+        private IEnumerator _flashing;
 
         private float _lastEatenTime;
 
         public int Amount => amount;
 
-        private void Start() =>
-            // Just set it such that a player can instantly eat it without delay to prevent need of null check
+        private void Start()
+        {
+            // Instantly edible
             _lastEatenTime = Time.time - eatDelay;
+            meshRenderer = meshRenderer ? meshRenderer : GetComponent<MeshRenderer>();
+            impulseSource = impulseSource ? impulseSource : GetComponent<CinemachineImpulseSource>();
+            
+            if (!meshRenderer) Debug.LogError("Material is required for this behavior");
+            if (!impulseSource) Debug.LogError("Cinemachine impulse source is required for this behavior");
+        }
 
         public bool CanBeEaten() => amount > 0 && Time.time - _lastEatenTime > eatDelay;
 
@@ -24,10 +39,25 @@ namespace Behaviors
             if (!CanBeEaten()) return;
 
             _lastEatenTime = Time.time;
+            meshRenderer.material.color = MakeMaterialLighter(meshRenderer.material.color);
+            _flashing = FlashingBehavior(meshRenderer.material.color);
+            StartCoroutine(_flashing);
             if (onBeEatenParticles) onBeEatenParticles.Play();
+            if (impulseSource) impulseSource.GenerateImpulse();
             amount -= 1;
             if (amount == 0)
                 Destroy(gameObject);
+        }
+        private Color MakeMaterialLighter(Color color) => Color.Lerp(color, Color.white, .1f);
+
+        private IEnumerator FlashingBehavior(Color originalColor)
+        {
+            for (int i = 0; i < flashCount; i++)
+            {
+                meshRenderer.material.color = Color.white;
+                yield return new WaitForSeconds(flashDelay);
+                meshRenderer.material.color = originalColor;
+            }
         }
 
         public Score Score => Score.Of(points);
