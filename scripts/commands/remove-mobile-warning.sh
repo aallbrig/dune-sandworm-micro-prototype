c56c01bf7b016961b2294498b20c061878817344
Andrew Allbright <aallbrig@gmail.com>
2022-02-18 13:49:00 -0500

    Unity pipeline produces fullscreen player, reachable by QR code

--- scripts/commands/remove-mobile-warning.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TARGET_FILE=${1:-"docs/WebGL/Build/WebGL.loader.js"}
-echo "TARGET_FILE: ${TARGET_FILE}"
-
-# Very Fragile
-sed -i -e 's_mobile:/Mobile|Android|iP(ad|hone)/.test(navigator.appVersion)_mobile:false_g' "${TARGET_FILE}"
